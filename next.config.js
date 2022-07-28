d58700b5b8eda103f89913441a38c501441aa049
Divvu1103 <200110075@hbtu.ac.in>
2022-07-28 17:03:23 +0530

    created

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'images.unsplash.com'
+    ],
+  },
+};
+
+module.exports = nextConfig;
